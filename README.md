a9f3dfc3b1ba8e1dffc63752cf1920ee879df2e8
Fahsai Sripaew <fahsai.sr@ip-10-30-118-248.ap-southeast-1.compute.internal>
2026-09-01 16:06:25 +0700

    add middleware and CORS

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go API - BornToDev
+
+This repository is part of the [Introduction to Go Programming Language](https://school.borntodev.com/course/introduction-to-go-programming-language) course by BornToDev.
---
+++ demo-CORS.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Course struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Price       float64 `json:"price"`
+	Instructor  string `json:"instructor"`
+}
+
+var CourseList []Course
+
+func init() {
+	CourseJSON := `[
+		{
+			"id": 1,
+			"name": "Golang",
+			"price": 100,
+			"instructor": "John"
+		},
+		{
+			"id": 2,
+			"name": "Java",
+			"price": 2000,
+			"instructor": "Doe"
+		}
+	]`
+	err := json.Unmarshal([]byte(CourseJSON), &CourseList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNextID() int {
+	hightestID := -1
+	for _, course := range CourseList {
+		if hightestID < course.Id {
+			hightestID = course.Id
+		}
+	}
+	return hightestID + 1
+}
+
+func findID(ID int) (*Course, int) {
+	for i, course := range CourseList {
+		if course.Id == ID {
+			return &course, i
+		}
+	}
+	return nil, 0
+}
+
+func courseHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegment := strings.Split(r.URL.Path, "course/")
+	ID, err := strconv.Atoi(urlPathSegment[len(urlPathSegment)-1])
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	course, listItemIndex := findID(ID)
+	if course == nil {
+		http.Error(w, fmt.Sprintf("no course with id %d",ID), http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+		case http.MethodGet:
+			courseJSON, err := json.Marshal(course)
+			if err != nil { 
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(courseJSON)
+		case http.MethodPut:
+			var updateCourse Course
+			bodyByte, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			err = json.Unmarshal(bodyByte, &updateCourse)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if updateCourse.Id != ID {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			course = &updateCourse
+			CourseList[listItemIndex] = *course
+			w.WriteHeader(http.StatusOK)
+			return
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+	}
+}
+
+func coursesHandler(w http.ResponseWriter, r *http.Request) {
+	courseJSON, err := json.Marshal(CourseList)
+
+	switch r.Method {
+	case http.MethodGet:
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(courseJSON)
+	case http.MethodPost:
+		var newCourse Course
+		BodyByte, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(BodyByte, &newCourse)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		newCourse.Id = getNextID()
+		CourseList = append(CourseList, newCourse)
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+}
+
+func enableCorsMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization, X-Requested-With, ngrok-skip-browser-warning")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	courseItem := http.HandlerFunc(courseHandler);
+	courseList := http.HandlerFunc(coursesHandler);
+	http.Handle("/course/", enableCorsMiddleware(courseItem))
+	http.Handle("/course", enableCorsMiddleware(courseList))
+	http.ListenAndServe(":5000", nil)
+}
